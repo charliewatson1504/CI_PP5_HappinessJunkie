f97a3f763b6550e14ebe37f885a0e7094525684d
Charlie Watson <charliewatson87@gmail.com>
2022-05-31 09:53:30 +0000

    Amended allowed hosts in settings.py

--- happiness_junkie/settings.py
+++ happiness_junkie/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["happiness-junkie.herokuapp.com","localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["happiness-junkie.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
