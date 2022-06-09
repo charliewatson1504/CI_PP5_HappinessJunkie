20a59556cfa247d0da31f53e172eb3aa254dae85
Charlie Watson <charliewatson87@gmail.com>
2022-06-09 12:18:07 +0000

    Create test for home app views

---
+++ home/test_views.py
@@ -0,0 +1,19 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.test import TestCase
+
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class TestIndexView(TestCase):
+    """
+    Class for testing views in home app
+    """
+    def test_get_index_page(self):
+        """
+        Test to check index page displays correctly
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
