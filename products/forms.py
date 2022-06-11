d0f82373fb11a52cd33cda163eb07a23fb5c96e5
Charlie Watson <charliewatson87@gmail.com>
2022-06-11 20:20:55 +0000

    fix error in form.py

--- products/forms.py
+++ products/forms.py
@@ -21,7 +21,7 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_names = [(c.id in c.get_friendly_name()) for c in categories]
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
