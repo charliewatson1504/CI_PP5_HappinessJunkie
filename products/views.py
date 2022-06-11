4d526fd8baed40118c6b02943846ea6e6b2ee348
Charlie Watson <charliewatson87@gmail.com>
2022-06-11 10:03:41 +0000

    Created all_products view in views.py - test failed

--- products/views.py
+++ products/views.py
@@ -1,3 +1,28 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.shortcuts import render
 
-# Create your views here.
+
+# Internal:
+from .models import Product
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+def all_products(request):
+    """
+    View to show all products available in store
+
+    Args:
+        request (object): HTTP request object
+    Returns:
+        Render of products page
+    """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/product.html', context)
