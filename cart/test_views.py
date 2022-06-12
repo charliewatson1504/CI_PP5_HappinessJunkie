4c6e1cd7a1a529b3ccc16df07da27874fcdd7da9
Charlie Watson <charliewatson87@gmail.com>
2022-06-12 11:59:30 +0000

    Created test for adding a product to cart

--- cart/test_views.py
+++ cart/test_views.py
@@ -2,6 +2,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 3rd party:
 from django.test import TestCase
+from django.contrib.messages import get_messages
 
 # Internal:
 from products.models import Product
@@ -37,3 +38,15 @@ class TestCartViews(TestCase):
         response = self.client.get('/cart/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'cart/cart.html')
+
+    def test_add_item_to_cart_no_variation(self):
+        """
+        Test adding a product to the cart that has no variations
+        """
+        product = Product.objects.get(sku='HJ8001234567')
+        response = self.client.post(
+            f'/cart/add/{product.id}/', {"quantity": 1, "redirect_url": "view_cart"})
+        cart = self.client.session['cart']
+        self.assertEqual(car[str(product.id)], 1)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]), 'Added to your cart!')
