11310b806632b5fa26d6a0263f5accb719b76b04
Charlie Watson <charliewatson87@gmail.com>
2022-06-13 17:56:01 +0000

    Created urls.py for profiles app and added paths

---
+++ profiles/urls.py
@@ -0,0 +1,17 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.urls import path
+
+
+# Internal:
+from . import views
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+urlpatterns = [
+    path('', views.profile, name='profile'),
+    path('order_history/<order_number>',
+         views.order_history,
+         name='order_history'),
+]
