3981771373cdce6f5b79517a95412b9ed498aed3
Charlie Watson <charliewatson87@gmail.com>
2022-06-13 22:15:04 +0000

    Created blog post list view in views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.shortcuts import render
+from django.views import generic, View
 
-# Create your views here.
+# Internal:
+from .models import Post, Comment
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class PostList(generic.ListView):
+    """
+    View for displaying list of blog posts
+    """
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog.html'
+    paginate_by = 4
