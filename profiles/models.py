7770762bdec07178225010aff7e66a38dea26568
Charlie Watson <charliewatson87@gmail.com>
2022-06-13 17:29:42 +0000

    Created UserProfile model in profile app and installed CountryField

--- profiles/models.py
+++ profiles/models.py
@@ -2,7 +2,55 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 3rd party:
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
 
 # Internal:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class UserProfile(models.Model):
+    """
+    User profile model to display and update default shipping
+    information and showing order history.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True
+    )
+    default_street_address1 = models.CharField(
+        max_length=80,
+        null=True,
+        blank=True
+    )
+    default_street_address2 = models.CharField(
+        max_length=80,
+        null=True,
+        blank=True
+    )
+    default_town_or_city = models.CharField(
+        max_length=40,
+        null=True,
+        blank=True
+    )
+    default_county = models.CharField(
+        max_length=80,
+        null=True,
+        blank=True
+    )
+    default_postcode = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True
+    )
+    default_country = CountryField(
+        blank_label='Country',
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
