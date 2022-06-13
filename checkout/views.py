638652b211017cfeff9a02571ee2e82a09cd5e33
Charlie Watson <charliewatson87@gmail.com>
2022-06-13 19:59:48 +0000

    Created checkout view for checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,96 @@
-from django.shortcuts import render
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+import json
 
-# Create your views here.
+from django.contrib import messages
+from django.shortcuts import redirect, reverse, render
+
+# Internal:
+from .forms import OrderForm
+from .models import OrderLineItem
+from products.models import Product
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+def checkout(request):
+    """
+    Checkout view for the user
+    """
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
+
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            order.original_cart = json.dumps(cart)
+            order.save()
+            for item_id, item_data in cart.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        if cart['items_by_zodiac_style']:
+                            for zodiac_style, quantity in item_data['items_by_zodiac_style'].items():
+                                order_line_item = OrderLineItem(
+                                    order=order,
+                                    product=product,
+                                    quantity=quantity,
+                                    product_zodiac_style=zodiac_style,
+                                )
+                                order_line_item.save()
+                        else:
+                            for foil_print_color, quantity in item_data['items_by_foil_print_color'].items():
+                                order_line_item = OrderLineItem(
+                                    order=order,
+                                    product=product,
+                                    quantity=quantity,
+                                    product_foil_print_color=foil_print_color,
+                                )
+                                order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't "
+                        "found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('cart'))
+
+            # Save the info to the user's profile if all is well
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success',
+                                    args=[order.order_number]))
+        messages.error(request, ('There was an error with your form. '
+                                 'Please double check your information.'))
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request,
+                           "There's nothing in your cart at the moment")
+            return redirect(reverse('products'))
+
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
