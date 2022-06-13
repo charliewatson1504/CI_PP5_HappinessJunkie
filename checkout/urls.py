af1ead15c051570f4a4efa59878b01e6b8d721d2
Charlie Watson <charliewatson87@gmail.com>
2022-06-13 20:12:50 +0000

    Created urls.py for checkout app

---
+++ checkout/urls.py
@@ -0,0 +1,14 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.urls import path
+
+
+# Internal:
+from . import views
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+urlpatterns = [
+    path('', views.checkout, name='checkout'),
+]
