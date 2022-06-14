c638d273cef53b8330519ca2405ec665674376d3
Charlie Watson <charliewatson87@gmail.com>
2022-06-14 01:03:03 +0000

    Created edit_blog_post.html

--- blog/urls.py
+++ blog/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('', views.all_posts, name='blog'),
     path('<int:post_id>/', views.view_post, name='blog_post'),
     path('add_blog_post/', views.add_blog_post, name='add_blog_post'),
+    path('edit_blog_post/<int:post_id>',
+         views.edit_blog_post, name='edit_blog_post'),
 ]
