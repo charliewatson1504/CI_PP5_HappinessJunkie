d9df145738223fc74e358d3e120373ddc7133803
Charlie Watson <charliewatson87@gmail.com>
2022-08-02 18:12:59 +0000

    FIX: issue when placing order in checkout view

--- happiness_junkie/settings.py
+++ happiness_junkie/settings.py
@@ -120,16 +120,16 @@ WSGI_APPLICATION = "happiness_junkie.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
-else:
+if 'DEVELOPMENT' in os.environ:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
 
