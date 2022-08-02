c9158176c887ed3ceb6e874f0547dfceec85802f
Charlie Watson <charliewatson87@gmail.com>
2022-08-02 08:38:18 +0000

    FIX: indentation error in settings.py

--- happiness_junkie/settings.py
+++ happiness_junkie/settings.py
@@ -212,8 +212,8 @@ STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
 STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
 
 if 'DEVELOPMENT' in os.environ:
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-DEFAULT_FROM_EMAIL = "happinessjunkie@example.com"
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "happinessjunkie@example.com"
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
