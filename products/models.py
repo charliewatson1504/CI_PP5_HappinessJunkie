ec122848c212b1e902d9325dc9c0bcfb5b71281f
Charlie Watson <charliewatson87@gmail.com>
2022-08-03 09:49:04 +0000

    Added model for star rating and updated product model

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 3rd party:
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Internal:
@@ -51,8 +52,68 @@ class Product(models.Model):
         default=False, null=True, blank=True)
     has_foil_print_color = models.BooleanField(
         default=False, null=True, blank=True)
+    star_rating = models.DecimalField(
+        verbose_name=('Star Rating'),
+        max_digits=6,
+        decimal_places=2,
+        default=0,
+        null=True,
+        blank=True
+    )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    """
+    Class for review model
+    """
+    class Meta:
+        ordering = ['-created_on']
+
+    STAR_CHOICES = [
+        (1, '*'),
+        (2, '**'),
+        (3, '***'),
+        (4, '****'),
+        (5, '*****')
+    ]
+
+    user = models.ForeignKey(
+        User,
+        null=True,
+        on_delete=models.SET_NULL
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    star_rating = models.IntegerField(
+        choices=STAR_CHOICES,
+        default=5
+    )
+
+    review_text = models.TextField(
+        verbose_name=_('Review Text'),
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    created_on = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        """
+        Returns review text
+
+        Returns:
+            Review text
+        """
+        return self.review_text
