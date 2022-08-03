1355bdf9f634d8a986a73f9ba98a07503283b4e1
Charlie Watson <charliewatson87@gmail.com>
2022-08-03 18:37:19 +0000

    Updated models for review functionality

--- products/models.py
+++ products/models.py
@@ -75,11 +75,11 @@ class Review(models.Model):
         ordering = ['-created_on']
 
     STAR_CHOICES = [
-        (1, '*'),
-        (2, '**'),
-        (3, '***'),
-        (4, '****'),
-        (5, '*****')
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5')
     ]
 
     user = models.ForeignKey(
