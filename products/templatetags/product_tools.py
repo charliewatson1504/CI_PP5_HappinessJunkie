473b2c41ebe5b4a05cd3ad54c9123f38b4393c48
Charlie Watson <charliewatson87@gmail.com>
2022-08-03 20:09:06 +0000

    Show average rating on all products page

--- products/templatetags/product_tools.py
+++ products/templatetags/product_tools.py
@@ -11,4 +11,5 @@ register = template.Library()
 
 @register.filter(name="format_rating")
 def format_rating(star_rating):
-    return '⭐️' * star_rating
+    formatted_star_rating = int(star_rating)
+    return '⭐️' * formatted_star_rating
